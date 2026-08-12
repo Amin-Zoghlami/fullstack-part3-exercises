f45aea1230a5045ca8964c3a55d384b9c8c84a62
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2026-08-12 03:24:02 -0700

    Finish exercises 2-6

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -24,8 +26,62 @@ let persons = [
   },
 ];
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+     <p>${new Date()}</p>`,
+  );
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  console.log(persons);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000000);
+  return String(id);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  } else if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  } else if (
+    persons.some(
+      (person) => person.name.toLowerCase() === body.name.toLowerCase(),
+    )
+  ) {
+    return response.status(400).json({ error: "name already exists" });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
