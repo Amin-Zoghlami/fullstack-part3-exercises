ac641178a31a4fdac98044209a8cdfa7d0e825d6
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2026-08-12 03:27:37 -0700

    Remove console.log

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -50,7 +50,6 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   persons = persons.filter((person) => person.id !== id);
-  console.log(persons);
   response.status(204).end();
 });
 
