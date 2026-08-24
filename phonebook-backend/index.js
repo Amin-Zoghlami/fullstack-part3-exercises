c3ca23a697330d5271b72a80f963d6009dc1fdc6
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2026-08-24 00:56:49 -0700

    Finish exercises 9-11

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.static("dist"));
-app.use(cors());
 app.use(express.json());
 
 morgan.token("content", (request, response) => JSON.stringify(request.body));
