d0bd9951b2983abfef3fbd39b5c6a1b38fe3384a
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2026-08-24 00:13:28 -0700

    Add environment variable port

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("content", (request, response) => JSON.stringify(request.body));
@@ -92,7 +94,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
